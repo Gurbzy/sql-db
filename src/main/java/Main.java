ecbbf802835ba8077571626e1b2e82aab81ab2ae
Tyler Cako <tylercakotc@gmail.com>
2026-07-13 20:34:56 -0600

    fixed pom

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
+        logger.info("Meow");
         System.out.println("Hello SQL World!");
     }
 }
